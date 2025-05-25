b4370a189821fd7d32a5535bc1a36c410e030bd4
eemlnv <emelin.ev@phystech.edu>
2025-05-25 17:12:28 +0000

    add rec service

---
+++ test_rec.py
@@ -0,0 +1,17 @@
+import requests
+
+recommendations_url = "http://127.0.0.1:8000/recommendations"
+
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+params = {"user_id": 0}
+
+resp = requests.post(recommendations_url, headers=headers, params=params)
+
+if resp.status_code == 200:
+    print("OK")
+    recs = resp.json()
+else:
+    recs = []
+    print(f"status code: {resp.status_code}")
+    
+print(recs)
